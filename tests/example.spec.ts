bf4b381a8c5fa400a053b18d0a0d5ba4bba26979
Steven Boutcher <boutchersj@gmail.com>
2024-12-29 01:08:42 -0600

    Share demo repo with students

---
+++ tests/example.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test';
+// import { searchForProduct } from './utils';
+
+// const productName = 'Red Rock Deli Lime And Pepper Potato Chips 165gm';
+// test('has title', async ({ page }) => {
+//   await page.goto('https://a.co/d/i755aa7');
+//   await expect(page.getByTestId('productTitle')).toBeVisible();
+//   await expect(page.getByTestId('productTitle')).toHaveText(productName);
+// });
+
+// test('product appears in search results', async ({ page }) => {
+//   await page.goto('https://a.co/d/i755aa7');
+//   await page.pause();
+//   const asinContainer = page.getByTestId('detailBullets_feature_div').getByText("ASIN").locator("xpath=following-sibling::*");
+//   const asin = await asinContainer.textContent() as string;
+//   await searchForProduct(page, asin);
+//   await expect(page.getByText(productName, { exact: true })).toBeVisible();
+// });
+
+test('can add product to cart', async ({ page }) => {
+  await page.goto('https://a.co/d/i755aa7');
+  const addToCartButton = page.getByTestId('add-to-cart-button');
+  const cartCount = page.getByTestId('nav-cart-count');
+  const gotoCartButton = page.getByTestId('sw-gtc');
+  const cartItemCount = page.getByRole('spinbutton');
+  const checkoutButton = page.getByTestId('desktop-ptc-button-celWidget').getByRole('button');
+
+  await addToCartButton.click();
+  await expect(cartCount).toHaveText('1');
+  await gotoCartButton.click();
+  await expect(cartItemCount).toHaveText('1');
+  await checkoutButton.click();
+  await expect(page).toHaveURL(/https:\/\/www\.amazon\.com\/ap\/signin.*/);
+});
---
+++ tests/utils.ts
@@ -0,0 +1,8 @@
+import { Page } from "@playwright/test";
+
+export async function searchForProduct(page: Page, searchQuery: string) {
+    const searchBar = page.getByTestId('twotabsearchtextbox');
+    await searchBar.fill(searchQuery);
+    await searchBar.press('Enter');
+    await page.waitForTimeout(2000);
+}
