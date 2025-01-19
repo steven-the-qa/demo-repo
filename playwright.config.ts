c9327b88a8e7f287642e6b3db5f2109657a9e935
Steven Boutcher <boutchersj@gmail.com>
2025-01-19 15:47:06 -0600

    Remove GH Actions workflow, we don't use it yet

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     testIdAttribute: 'id',
-    headless: true,
+    headless: false,
   },
 
   /* Configure projects for major browsers */
